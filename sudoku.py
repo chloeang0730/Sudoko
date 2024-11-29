128bcf5858fa9c48a6d48e2295f129221b9584d1
chloeang0730 <130808941+chloeang0730@users.noreply.github.com>
2024-11-29 10:32:21 +0800

    add sudoku project file

---
+++ sudoku.py
@@ -0,0 +1,617 @@
+small = [[1, 0, 0, 0],
+         [0, 4, 1, 0],
+         [0, 0, 0, 3],
+         [4, 0, 0, 0]]
+
+small2 = [[0, 0, 1, 0],
+          [4, 0, 0, 0],
+          [0, 0, 0, 2],
+          [0, 3, 0, 0]]
+
+big = [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [4, 0, 0, 7, 8, 9, 0, 0, 0],
+       [7, 8, 0, 0, 0, 0, 0, 5, 6],
+       [0, 2, 0, 3, 6, 0, 8, 0, 0],
+       [0, 0, 5, 0, 0, 7, 0, 1, 0],
+       [8, 0, 0, 2, 0, 0, 0, 0, 5],
+       [0, 0, 1, 6, 4, 0, 9, 7, 0],
+       [0, 0, 0, 9, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 3, 0, 0, 0, 2]]
+
+big2 = [[7, 0, 0, 0, 0, 0, 0, 1, 0],
+        [0, 5, 0, 0, 0, 9, 0, 0, 0],
+        [8, 0, 0, 0, 3, 0, 0, 4, 0],
+        [0, 0, 0, 7, 6, 0, 0, 0, 8],
+        [6, 2, 0, 0, 5, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 3, 0, 7, 0],
+        [0, 0, 0, 6, 0, 0, 9, 8, 0],
+        [0, 0, 0, 0, 2, 7, 3, 0, 0],
+        [0, 0, 2, 0, 8, 0, 0, 5, 0]]
+
+big3 = [[0, 0, 8, 1, 9, 0, 0, 0, 6],
+        [0, 4, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 7, 6, 0, 0, 1, 3, 0],
+        [0, 0, 6, 0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 0, 8, 0, 0, 0, 0],
+        [4, 0, 0, 0, 0, 2, 0, 0, 5],
+        [0, 0, 0, 0, 3, 0, 9, 0, 0],
+        [0, 1, 0, 4, 0, 0, 0, 0, 2],
+        [0, 0, 0, 0, 0, 0, 0, 5, 7]]
+
+big4 = [[0, 0, 0, 6, 0, 0, 2, 0, 0],
+        [8, 0, 4, 0, 3, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 9, 0, 0, 0],
+        [4, 0, 5, 0, 0, 0, 0, 0, 7],
+        [7, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 3, 0, 5, 0, 0, 0, 8],
+        [3, 0, 0, 0, 7, 0, 0, 0, 4],
+        [0, 0, 0, 0, 0, 1, 9, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 6, 0]]
+
+giant = [[ 0,  0, 13,  0,  0,  0,  0,  0,  2,  0,  8,  0,  0,  0, 12, 15],
+         [ 7,  8, 12,  2, 10,  0,  0, 13,  0,  0, 14, 11,  6,  9,  0,  4],
+         [11, 10,  0,  0,  0,  6, 12,  5,  0,  3,  0,  0,  0, 14,  0,  8],
+         [ 1,  0,  0,  0, 14,  0,  2,  0,  0,  4,  6,  0, 16,  3,  0, 13],
+         [12,  6,  0,  3,  0,  0, 16, 11,  0, 10,  1,  7, 13, 15,  0,  0],
+         [ 0, 13,  0,  0,  0, 15,  8,  0, 14,  0,  0,  0,  0, 16,  5, 11],
+         [ 8,  0, 11,  9, 13,  0,  7,  0,  0,  0,  0,  3,  2,  4,  0, 12],
+         [ 5,  0,  0, 16, 12,  9,  0, 10, 11,  2, 13,  0,  0,  0,  8,  0],
+         [ 0,  0,  0,  0, 16,  8,  9, 12,  0,  0,  0,  0,  0,  6,  3,  0],
+         [ 2, 16,  0,  0,  0, 11,  0,  0,  7,  0, 12,  6,  0, 13, 15,  0],
+         [ 0,  0,  4,  0,  0, 13,  0,  7,  3, 15,  0,  5,  0,  0,  0,  0],
+         [ 0,  7,  0, 13,  4,  5, 10,  0,  1,  0, 11, 16,  9,  0, 14,  2],
+         [ 0,  2,  8,  0,  9,  0,  0,  0,  4,  0,  7,  0,  0,  5,  0,  0],
+         [14,  0,  0,  0, 15,  2, 11,  4,  9, 13,  3,  0, 12,  0,  0,  0],
+         [ 0,  1,  9,  7,  0,  0,  5,  0,  0, 11, 15, 12,  0,  0,  0,  0],
+         [16,  3, 15,  0,  0, 14, 13,  6, 10,  1,  0,  2,  0,  8,  4,  9]]
+
+giant2 = [[ 0,  5,  0,  0,  0,  4,  0,  8,  0,  6,  0,  0,  0,  0,  9, 16],
+          [ 1,  0,  0,  0,  0,  0,  0, 13,  4,  0,  0,  7, 15,  0,  8,  0],
+          [13,  0,  0,  0,  0,  7,  3,  0,  0,  0,  0,  9,  5, 10,  0,  0],
+          [ 0, 11, 12, 15, 10,  0,  0,  0,  0,  0,  5,  0,  3,  4,  0, 13],
+          [15,  0,  1,  3,  0,  0,  7,  2,  0,  0,  0,  0,  0,  5,  0,  0],
+          [ 0,  0,  0, 12,  0,  3,  0,  5,  0, 11,  0, 14,  0,  0,  0,  9],
+          [ 4,  7,  0,  0,  0,  0,  0,  0, 12,  0, 15, 16,  0,  0,  0,  0],
+          [ 0,  0,  0,  0, 14,  0, 15,  0,  6,  9,  0,  0,  0,  0, 12,  0],
+          [ 3,  0, 15,  4,  0, 13, 14,  0,  0,  0,  0,  1,  0,  0,  7,  8],
+          [ 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  9, 10,  0,  0,  0,  0],
+          [11,  0, 16, 10,  0,  0,  0,  0,  0,  7,  0,  0,  0,  3,  5,  0],
+          [ 0,  0, 13,  0,  0,  0,  0,  0, 14,  0, 16, 15,  0,  9,  0,  1],
+          [ 9,  0,  2,  0,  0, 14,  0,  4,  8,  0,  0,  0,  0,  0,  0,  0],
+          [ 0, 14,  0,  0,  0,  0,  0, 10,  9,  0,  3,  0,  0,  0,  1,  7],
+          [ 8,  0,  0,  0, 16,  0,  0,  1,  2, 14, 11,  4,  0,  0,  0,  3],
+          [ 0,  0,  0,  1,  0,  0,  5,  0,  0, 16,  0,  6,  0, 12,  0,  0]]
+
+giant3 = [[ 0,  4,  0,  0,  0,  0,  0, 12,  0,  1,  0,  0,  9,  0,  8,  0],
+          [15, 14,  0,  0,  9,  0,  0, 13,  8,  0,  0, 10,  1,  0,  0,  0],
+          [ 0,  7,  0,  0,  0,  0,  0,  8, 16,  0, 14,  0,  0,  2,  0,  0],
+          [ 0,  0,  0,  9,  0,  0, 11,  0,  0,  0,  0,  0,  5,  0,  0, 15],
+          [ 3,  0, 12,  0,  7,  0, 10,  0,  0, 11,  2,  0,  0,  0,  0,  6],
+          [14,  8,  0,  0,  0, 12,  0,  6,  0,  0,  0, 16,  0,  0,  0, 10],
+          [ 0, 16,  0,  0, 13,  0,  0,  0,  0,  0,  0,  0,  0,  0, 12,  0],
+          [ 6,  0,  0,  0,  0,  8,  0,  5,  1,  7, 13,  0, 11,  0,  0, 14],
+          [ 0,  0,  0,  2,  0,  0, 16,  0, 15, 12,  0,  3, 10,  7,  0,  0],
+          [ 0,  9,  0,  5, 11,  0,  3,  0,  4, 13, 16,  0,  0, 15,  6,  0],
+          [ 0,  0,  0,  0,  5,  4,  0,  0,  9,  6,  0,  2,  0,  0,  0,  0],
+          [ 1,  0,  0,  0,  0, 15, 12,  0,  0,  0,  5,  0,  0,  0,  9,  0],
+          [12, 10,  0, 15,  0,  1,  0,  0,  2,  9,  3,  4,  0,  0,  5,  0],
+          [ 0,  0,  0,  3, 10,  0,  4,  0,  0, 15,  0,  0,  0,  0,  0,  0],
+          [ 0,  0,  0,  0, 16,  0,  0,  0,  0,  0,  0,  0,  0,  0, 10, 11],
+          [11,  6,  8,  0,  0,  0, 15,  0, 14,  0,  0,  0,  0, 13,  0,  2]]
+
+sudokus = [[], [], [small, small2], [big, big2, big3, big4], [giant, giant2, giant3]]
+
+########### Module functions ###########################
+
+def print_board(board):
+    """
+    Prints a given board to the console in a way that aligns the content of columns and makes
+    the subgrids visible.
+
+    Input : a Sudoku board (board) of size 4x4, 9x9, or 16x16
+    Effect: prints the board to the console 
+
+    For example:
+
+    >>> print_board(small2)
+    -------
+    |  |1 |
+    |4 |  |
+    -------
+    |  | 2|
+    | 3|  |
+    -------
+    >>> print_board(big)
+    -------------
+    |   |   |   |
+    |4  |789|   |
+    |78 |   | 56|
+    -------------
+    | 2 |36 |8  |
+    |  5|  7| 1 |
+    |8  |2  |  5|
+    -------------
+    |  1|64 |97 |
+    |   |9  |   |
+    |   | 3 |  2|
+    -------------
+    >>> print_board(giant2)
+    ---------------------
+    | 5  | 4 8| 6  |  9G|
+    |1   |   D|4  7|F 8 |
+    |D   | 73 |   9|5A  |
+    | BCF|A   |  5 |34 D|
+    ---------------------
+    |F 13|  72|    | 5  |
+    |   C| 3 5| B E|   9|
+    |47  |    |C FG|    |
+    |    |E F |69  |  C |
+    ---------------------
+    |3 F4| DE |   1|  78|
+    |    |    |  9A|    |
+    |B GA|    | 7  | 35 |
+    |  D |    |E GF| 9 1|
+    ---------------------
+    |9 2 | E 4|8   |    |
+    | E  |   A|9 3 |  17|
+    |8   |G  1|2EB4|   3|
+    |   1|  5 | G 6| C  |
+    ---------------------
+    """
+    
+    import math
+    n=len(board)
+    k=int(math.sqrt(n))
+    x=len(board)+k+1
+    y='-'*x
+    
+    row_var=-1
+    for row in board:
+        row_var+=1
+        if row_var%k==0:
+            print(y)
+
+        string=''
+        col_var=0
+        for i in range(0,len(row)):
+            col_var+=i
+            if col_var%k==0: 
+                string+='|'
+            col_var=0
+            if row[i]in range(1,10):
+                string+=str(row[i])
+            elif row[i]==0:
+                string+=" "
+            elif row[i]<=15:
+                string+=str(hex(row[i])[-1].upper())
+            else:
+                string+='G'
+            
+        print(string+'|')
+    print(y)
+    
+    
+
+
+def subgrid_values(board, r, c):
+    """
+    Input : Sudoku board (board), row index (r), and column index (c)
+    Output: list of all numbers that are present in the subgrid of the board related to the
+            field (r, c)
+
+    For example:
+
+    >>> subgrid_values(small2, 1, 3)
+    [1]
+    >>> subgrid_values(big, 4, 5)
+    [3, 6, 7, 2]
+    >>> subgrid_values(giant2, 4, 5)
+    [7, 2, 3, 5, 14, 15]
+    """
+    import math
+    n = len(board)
+    k = round(math.sqrt(n))
+    res = []
+    for i in range((r // k) * k, ((r // k) + 1) * k):
+        for j in range((c // k) * k, ((c // k) + 1) * k):
+            if board[i][j]:
+                res.append(board[i][j])
+    
+    return res
+
+
+
+def options(board, r, c):
+    """
+    Input : Sudoku board (board), row index (r), and column index (c)
+    Output: list of all numbers that player is allowed to place on field (r, c),
+            i.e., those that are not already present in row r, column c,
+            and subgrid related to field (r, c)
+
+    For example:
+
+    >>> options(small2, 0, 0)
+    [2, 3]
+    >>> options(big, 6, 8)
+    [3, 8]
+    >>> options(giant2, 1, 5)
+    [2, 5, 6, 9, 11, 12, 16]
+    """
+    if board[r][c]:
+        return []
+    import math
+    res = []
+    n = len(board)
+    k = round(math.sqrt(n))    
+    col_vals = [board[s][c] for s in range(n)]
+    row_vals = board[r]
+    subgrid_vals = subgrid_values(board, r, c)
+    for x in range(1, n+1):
+        if x not in col_vals and x not in row_vals and x not in subgrid_vals:
+            res.append(x)
+    return res
+
+
+
+def solved_board(board):
+    if any(0 in i for i in board):
+        return False
+    else:
+        return True
+
+            
+
+def play(board):
+    """
+    Input : Sudoku board
+    Effect: Allows user to play board via console
+    """
+    import copy
+    temp=copy.deepcopy(board)
+    lst=[0,0,0]
+    print_board(board)
+    while True:
+        if solved_board(board)==True:
+            print("solve!")
+            break
+        inp = input().split(' ')
+        if len(inp) == 3 and inp[0].isdecimal() and inp[1].isdecimal() and inp[2].isdecimal():
+            i = int(inp[0])
+            j = int(inp[1])
+            x = int(inp[2])
+            if x in options(board,i,j):
+                board[i][j] = x
+                lst[0]=i
+                lst[1]=j
+                lst[2]=x
+                print_board(board)
+            else:
+                print("error!")
+        elif len(inp)==3 and (inp[0] == 'n' or inp[0] == 'new') and inp[1].isdecimal() and inp[2].isdecimal():
+            k = int(inp[1])
+            d = int(inp[2])
+            if k < len(sudokus) and 0 < d <= len(sudokus[k]):
+                board = sudokus[k][d-1]
+                print_board(board)
+            else:
+                print('board not found')
+        elif inp[0] == 'q' or inp[0] == 'quit':
+            return
+        elif inp[0]=='u' or inp[0]=='undo':
+            board[i][j]=0
+            print_board(board)
+
+        elif inp[0]=='r' or inp[0]=='restart':
+            board=temp
+            print_board(temp)
+             
+        elif inp[0]=='i':
+            w=inferred(board)
+            print_board(w)
+
+        elif inp[0]=='s' or inp[0]=='solve':
+            s=last1(board)
+            
+               
+        else:
+            print('Invalid input')
+
+    
+
+            ########### Functions only relevant for Part II ########
+
+def value_by_single(board, i, j):
+    """
+    Input : board, row, and column index
+    Output: The correct value for field (i, j) in board if it can be inferred as
+            either a forward or a backward single; or None otherwise. 
+    
+    For example:
+
+    >>> value_by_single(small2, 0, 1)
+    2
+    >>> value_by_single(small2, 0, 0)
+    3
+    >>> value_by_single(big, 0, 0)
+    """
+    v=options(board,i,j)
+    
+    #forward-single
+    if len(v)==1:
+        return v[0]
+
+    #backward-single
+    #row
+    rolop=[]
+    for r in range(len(board)):
+        if board[i][r]==0:
+            rolop+=options(board,i,r)
+    
+    
+    colop=[]
+    for c in range(len(board)):
+        if board[c][j]==0:
+            colop+=options(board,c,j)
+    
+    
+    
+    import math
+    n=len(board)
+    k=int(math.sqrt(n))
+    lst=[]
+    m=0
+    l=0
+    if i%k!=0:
+        m=i%k
+    else:
+        m=0
+    if j%k!=0:
+        l=j%k
+    else:
+        l=0
+    subop=[]
+    for rol in range(i-m,i-m+k):
+        for col in range(j-l,j-l+k):
+            if board[rol][col]==0:
+                subop+=options(board,rol,col)
+    
+    for a in v:
+       if rolop.count(a)==1:
+           return a
+    for a in v:
+        if colop.count(a)==1:
+            return a
+    for a in v:
+        if subop.count(a)==1:
+            return a
+    return None
+
+
+        
+                   
+                
+            
+
+
+        
+        
+        
+    
+
+
+def inferred(board):
+    """
+    Input : Sudoku board
+    Output: new Soduko board with all values field from input board plus
+            all values that can be inferred by repeated application of 
+            forward and backward single rule
+
+    For example board big can be completely inferred:
+
+    >>> inferred(big) # doctest: +NORMALIZE_WHITESPACE
+    [[2, 1, 3, 4, 5, 6, 7, 8, 9],
+    [4, 5, 6, 7, 8, 9, 1, 2, 3],
+    [7, 8, 9, 1, 2, 3, 4, 5, 6],
+    [1, 2, 4, 3, 6, 5, 8, 9, 7],
+    [3, 6, 5, 8, 9, 7, 2, 1, 4],
+    [8, 9, 7, 2, 1, 4, 3, 6, 5],
+    [5, 3, 1, 6, 4, 2, 9, 7, 8],
+    [6, 4, 2, 9, 7, 8, 5, 3, 1],
+    [9, 7, 8, 5, 3, 1, 6, 4, 2]]
+
+    But function doesn't modify input board:
+
+    >>> big # doctest: +NORMALIZE_WHITESPACE
+    [[0, 0, 0, 0, 0, 0, 0, 0, 0],
+     [4, 0, 0, 7, 8, 9, 0, 0, 0],
+     [7, 8, 0, 0, 0, 0, 0, 5, 6],
+     [0, 2, 0, 3, 6, 0, 8, 0, 0],
+     [0, 0, 5, 0, 0, 7, 0, 1, 0],
+     [8, 0, 0, 2, 0, 0, 0, 0, 5], 
+     [0, 0, 1, 6, 4, 0, 9, 7, 0],
+     [0, 0, 0, 9, 0, 0, 0, 0, 0],
+     [0, 0, 0, 0, 3, 0, 0, 0, 2]]
+
+    In board big4 there is nothing to infer:
+    
+    >>> inferred(big4) # doctest: +NORMALIZE_WHITESPACE
+    [[0, 0, 0, 6, 0, 0, 2, 0, 0],
+     [8, 0, 4, 0, 3, 0, 0, 0, 0],
+     [0, 0, 0, 0, 0, 9, 0, 0, 0], 
+     [4, 0, 5, 0, 0, 0, 0, 0, 7],
+     [7, 1, 0, 0, 0, 0, 0, 0, 0],
+     [0, 0, 3, 0, 5, 0, 0, 0, 8],
+     [3, 0, 0, 0, 7, 0, 0, 0, 4],
+     [0, 0, 0, 0, 0, 1, 9, 0, 0],
+     [0, 0, 0, 2, 0, 0, 0, 6, 0]]
+    
+    """
+    import copy
+    d=copy.deepcopy(board)
+    
+    n=0
+    for i in board:
+        for j in i:
+            if j==0:
+                n+=1
+    h=0
+    
+    temp=d
+    while h<n :
+        temp=d
+        for a in range(len(d)):
+            for b in range(len(d)):
+                if d[a][b]==0:
+                    p=value_by_single(temp,a,b)
+                    if p is not None:
+                        d[a][b]=p
+                    else:
+                        d[a][b]=0
+        h+=1
+    return d
+
+
+        
+        
+        
+    
+
+def empty_fields(board):
+    """
+    Input : Sudoku board
+    Output: list of fields, i.e., pairs of row and column indices, that are not
+            empty (i.e., value is not equal to 0)
+    """
+    import math
+    n = len(board)
+    k = int(math.sqrt(n))
+    res = []
+    for i in range(n):
+        for j in range(n):
+            if not board[i][j]:
+                res.append((i,j))
+    return res
+          
+def smart(board):
+  
+    fields = empty_fields(board)
+    
+    if not fields:
+        return None
+    min_field = fields[0]
+    min_options = len(options(board, fields[0][0], fields[0][1]))
+    for i, j in fields[1:]:
+        opts = options(board, i, j)
+        if len(opts) < min_options:
+            min_field = (i, j)
+            min_options = len(opts)
+    return min_field            
+
+def last1(board):
+    T=inferred(board)
+    if solved_board(T):
+        #print_board(T)
+        return T
+                
+                
+    v=smart(T)
+    if v==None:
+        return board
+
+    i=v[0]
+    j=v[1]
+    
+    #Updated here 
+    for opt in options(T, i, j):
+        T[i][j]=opt
+        if last1(T):
+            return last1(T)
+        else:
+            T[i][j]=0
+    return False
+
+    # for k in range(1,len(board)+1):
+    #     P=options(T,i,j)
+    #     if k in P:
+    #         T[i][j]=k
+    #         if last1(T):
+    #             return last1(T)
+           
+    #         else:
+    #             T[i][j]=0
+    # return False
+
+    
+    
+
+def last2(board):
+    import copy
+    T=inferred(board)
+    for k in range(len(T)):
+        for l in range(len(T)):
+            if solved_board(T):
+                
+                return T
+                
+          
+    v=smart(T)
+    if v==None:
+        return board 
+    i=v[0]
+    j=v[1]
+    P=options(board,i,j)
+    res=[]
+    for p in P:
+        c=copy.deepcopy(T)
+        c[i][j]=p
+        
+        res+=last2(c)
+    return res
+        
+print(last1(giant3))
+                   
+        
+    
+    
+        
+             
+                    
+            
+    
+        
+    
+              
+    
+
+ 
+    
+
+    
+
+
+                
+    
+                    
+        
+
+    
+        
+        
+    
+            
+        
+        
+             
+    
+               
+            
+        
+
+
+
+
+                
+       
+              
+          
+         
